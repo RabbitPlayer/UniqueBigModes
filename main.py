5165ca4ecec2166144187d68b49081c54532132a
RabbitPlayer <169180034+RabbitPlayer@users.noreply.github.com>
2024-05-09 14:01:17 +0000

    i rewrited the code

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("hello world")
+from PIL import Image, ImageDraw, ImageFont
+
+print("Генератор мемов запущен!")
+
+top_text = input("Введите вверхний текст: ")
+bottom_text = input("Введите нижний текст: ")
+
+print(top_text,bottom_text)
+
+print("Список картинок:")
+print("1. я не знаю")
+print("2. железный человек")
+print("3. ярость")
+
+image_number = int(input("Введите номер картинки: "))
+
+if image_number == 1:
+	image_file = "hz.png"
+elif image_number == 2:
+	image_file = "ironman.png"
+elif image_number == 3:
+	image_file = "yarost.png"
+image = Image.open(image_file)
+width, height = image.size
+
+draw = ImageDraw.Draw(image)
+
+font = ImageFont.truetype('arial.ttf',size=30)
+
+text = draw.textbbox((0,0),top_text, font)
+text_width = text[2]
+draw.text(((width - text_width) / 2,10), top_text, font=font, fill='black')
+
+text = draw.textbbox((0,0),bottom_text, font)
+text_width = text[2]
+text_height = text[3]
+draw.text(((width - text_width) / 2,height - text_height - 10), bottom_text, font=font, fill='black')
+
+image.save("new_meme.png")
